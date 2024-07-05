96767d8a1396e49265dc09f582cea2adbaaed73c
dfneto <pdavidfneto@gmail.com>
2024-07-05 15:41:43 +0200

    comentário sobre cores

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: davifern <davifern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 13:04:23 by davifern          #+#    #+#             */
-/*   Updated: 2024/07/05 15:23:27 by davifern         ###   ########.fr       */
+/*   Updated: 2024/07/05 15:40:39 by davifern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,9 +32,22 @@ void	my_mlx_pixel_put(t_img *img, int x, int y, int color)
 	*(unsigned int*)dst = color;
 }
 
+/* ver sobre cores e estudar bitshft e hexadecimal em:
+https://harm-smits.github.io/42docs/libs/minilibx/colors.html
+https://gontjarow.github.io/MiniLibX/mlx-tutorial-draw-pixel.html
+https://gontjarow.github.io/MiniLibX/mlx_pixel_put.html
+int rgb_to_int(double r, double g, double b)
+{
+    int color = 0;
+    color |= (int)(b * 255);
+    color |= (int)(g * 255) << 8;
+    color |= (int)(r * 255) << 16;
+    return (color);
+}
+*/
+
 int main(int argc, char **argv)
 {
-    // t_cub   *cub;
     t_win	window;
     t_img   img;
 
@@ -42,6 +55,7 @@ int main(int argc, char **argv)
 (void)argv;
     if (parse_map())
         return (err(ERROR_PARSING));
+    // https://harm-smits.github.io/42docs/libs/minilibx/getting_started.html#initialization
     window.mlx_ptr = mlx_init();
 	if (!window.mlx_ptr)
 		return (1);
@@ -51,7 +65,11 @@ int main(int argc, char **argv)
 	img.img_ptr = mlx_new_image(window.mlx_ptr, WIDTH, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img_ptr, &img.bpp,
 			&img.line_len, &img.endian);
-    my_mlx_pixel_put(&img, 150, 150, 0x00FF0000);
+    my_mlx_pixel_put(&img, 150, 150, 0xFF0000);
+    my_mlx_pixel_put(&img, 151, 150, 0xFF0000);
+    my_mlx_pixel_put(&img, 152, 150, 0xFF0000);
+    my_mlx_pixel_put(&img, 153, 150, 0xFF0000);
+    my_mlx_pixel_put(&img, 154, 150, 0xFF0000);
     mlx_put_image_to_window(window.mlx_ptr, window.win_ptr, img.img_ptr, 0, 0);
 
     mlx_loop(window.mlx_ptr);
