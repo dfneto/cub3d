6e64531e9225ced42534be6fa6fa592cc5b88cd5
David linux 42 <dfernandes8610@gmail.com>
2024-08-28 14:36:05 +0200

    refactoring

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: davifern <davifern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 13:04:23 by davifern          #+#    #+#             */
-/*   Updated: 2024/08/28 10:44:47 by davifern         ###   ########.fr       */
+/*   Updated: 2024/08/28 14:35:52 by davifern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,19 @@
 
 void	init_data(t_data *data, char *argv)
 {
+	data->textures = (t_texture *)safe_calloc(1, sizeof(t_texture)); //TODO: necessario um safe calloc?
+	data->ray = (t_ray *)safe_calloc(1, sizeof(t_ray));
 	init_window_and_image(data);
 	parse_input(argv, data);
-/* 	print_map_grid(data);
-	invert_grid(data);
-	print_map_grid(data); */
-	//init_map(data, argv);
 	init_player(data);
-	//init_texture(data);
 }
 
 int	main(int argc, char *argv[])
 {
 	t_data	data;
 
-	//VER COMO FAZER PARA INICIAR AS ESTRUTURAS
-	ft_bzero(&data, sizeof(t_data)); //TODO: necessario??
-	data.textures = (t_texture *)safe_calloc(1, sizeof(t_texture));
-	data.ray = (t_ray *)safe_calloc(1, sizeof(t_ray));
+	//VER COMO FAZER PARA INICIAR AS ESTRUTURAS Lucas
+	ft_bzero(&data, sizeof(t_data)); //TODO: necessario?? Lucas
 	if (argc != 2)
 		ft_perror(EINVAL, "./cub3d [MAP.cub]");
 	init_data(&data, argv[1]);
