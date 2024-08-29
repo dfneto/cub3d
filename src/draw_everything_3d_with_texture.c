ca661e37267a3f3977a774265f5990b5b426d092
dfneto <pdavidfneto@gmail.com>
2024-08-29 09:40:51 +0200

    load the textures accordingly to the side

--- src/draw_everything_3d_with_texture.c
+++ src/draw_everything_3d_with_texture.c
@@ -6,7 +6,7 @@
 /*   By: davifern <davifern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/05 18:07:08 by davifern          #+#    #+#             */
-/*   Updated: 2024/08/28 15:54:08 by davifern         ###   ########.fr       */
+/*   Updated: 2024/08/29 09:39:07 by davifern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,9 @@
 //meaning that it treats the memory at ptr as if it were storing an unsigned int.
 //(unsigned int *)ptr dereferences the pointer, giving you 
 //the unsigned int value stored at that memory location.
+//64 é o tamanho da textura. Para que o y cresça da mesma forma que num plano cartesiano
+//(y cresce à medida que sobe e não o contrário como ocorre na minilibx) tenho que printar
+//o y invertido (maior y - y atual)
 unsigned int	get_texture_pixel(t_img *texture, int pixel_x, int pixel_y)
 {
     pixel_y = 64 - pixel_y;
@@ -34,7 +37,7 @@ unsigned int	get_texture_pixel(t_img *texture, int pixel_x, int pixel_y)
 	return (*(unsigned int *)((texture->addr + pos)));
 }
 
-void drawBuffer(unsigned int buffer[HEIGHT][WIDTH], t_img *img)
+void render_buffer(unsigned int buffer[HEIGHT][WIDTH], t_img *img)
 {
     int y = 0;
     int x = 0;
@@ -63,6 +66,17 @@ void    clean_buffer(unsigned int buffer[HEIGHT][WIDTH])
             buffer[y][x++] = 0;
         y++;
     }
+}
+
+t_img   *define_wall_texture(t_data *data)
+{
+    if(data->ray->side == 0 && data->ray->rayDirX >= 0) 
+        return data->textures->east;
+    if(data->ray->side == 0 && data->ray->rayDirX < 0) 
+        return data->textures->west;
+    if(data->ray->side == 1 && data->ray->rayDirY >= 0)
+        return data->textures->north;
+    return data->textures->south;
 }
 
 /*
@@ -96,13 +110,11 @@ void    draw_everything_3d_texture(t_data *data)
         ray->mapY = (int)player->pos_y;
         calculate_side_dist_and_step(ray, player);
         while (ray->hit == 0)
-            calculate_side_dist_wall_position_and_side(ray, grid_map);
+            calculate_side_dist_ray_position_and_side(ray, grid_map); //dda
         calculate_wall_distance(ray);
 
         lineHeight = (int)(HEIGHT / ray->wall_distance);
-
         pitch = 0; 
-        
         //calculate lowest and highest pixel to fill in current stripe
         int drawStart = -lineHeight / 2 + HEIGHT / 2 + pitch;
         if(drawStart < 0) 
@@ -121,10 +133,8 @@ void    draw_everything_3d_texture(t_data *data)
 
         //x coordinate on the texture
         int texX = (int)(wallX * (double)texWidth); //texX eh o x da textura equivalente a onde bateu o raio na parede.
-        if(ray->side == 0 && ray->rayDirX > 0) texX = texWidth - texX - 1;
-        if(ray->side == 1 && ray->rayDirY < 0) texX = texWidth - texX - 1;
+        if(ray->side == 0) texX = texWidth - texX - 1; //sem isso fica invertido o west e east
         
-        // TODO: an integer-only bresenham or DDA like algorithm could make the texture coordinate stepping faster
         // How much to increase the texture coordinate per screen pixel: porque veja, eu tenho uma textura de
         // texHeight (64px no caso) de altura e tenho que desenhar uma linha vertical na tela (que vai ser o 3D do raycasting)
         // que pode ser maior ou menor do que esses 64px. Não basta copiar uma coluna da textura e colocar na tela.
@@ -139,18 +149,21 @@ void    draw_everything_3d_texture(t_data *data)
             int texY = (int)texPos & (texHeight - 1);
             texPos += step;
             //acho que aqui tenho que pegar o pixel da imagem da textura
-
-            unsigned int texture_pixel = get_texture_pixel(data->textures->north, texX, texY); //textures[texNum][texHeight * texY + texX]; //tenho que somar texX porque texture[texNum] eh um array e não uma matriz, entao essa eh a forma de pegar o pixel y,x (linha,coluna) da textura
+            t_img *texture = define_wall_texture(data);
+            unsigned int texture_pixel = get_texture_pixel(texture, texX, texY); //textures[texNum][texHeight * texY + texX]; //tenho que somar texX porque texture[texNum] eh um array e não uma matriz, entao essa eh a forma de pegar o pixel y,x (linha,coluna) da textura
             //make color darker for y-sides: R, G and B byte each divided through two with a "shift" and an "and"
             if(ray->side == 1) 
                 texture_pixel = (texture_pixel >> 1) & 8355711;
             buffer[y][x] = texture_pixel;
         }
+
+
+
         color_floor(data, buffer, drawStart, x);
         color_ceiling(data, buffer, drawEnd, x);
         x++;
     }
-    drawBuffer(buffer, data->img);
+    render_buffer(buffer, data->img);
     clean_buffer(buffer); //TODO: não sei se é necessário
 }
 
@@ -184,11 +197,12 @@ void calculate_ray_direction_and_delta(t_ray *ray, t_player *player)
 /*
 side_distance: get incremented with deltaDistX with every jump in 
 their direction, and mapX and mapY get incremented with stepX and stepY respectively. 
-map_x and map_y: are the ray position and get incremented with step_x and step_y respectively
+map_x and map_y: are the grid coordinates of the ray position and 
+get incremented with step_x and step_y respectively
 side: 1 if the ray intersects a NS grid line (y-side), 
 0 if the ray intersects a EW grid line (x-side)
 */
-void calculate_side_dist_wall_position_and_side(t_ray * ray, char **grid_map)
+void calculate_side_dist_ray_position_and_side(t_ray * ray, char **grid_map)
 {
     if (ray->sideDistX < ray->sideDistY)
     {
@@ -208,7 +222,7 @@ void calculate_side_dist_wall_position_and_side(t_ray * ray, char **grid_map)
 
 /*
 Calculates once, per x position:
-side_distance: are the distance the ray has to travel from 
+side_distance: is the distance the ray has to travel from 
 its start position to the first x-side and the first y-side
 step_x and step_y: always either -1 or +1. If the ray has 
 to go in the negative or positive direction
